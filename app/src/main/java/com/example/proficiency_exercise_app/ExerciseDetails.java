172bc21d5b5eeed8fe4cdb0a2848a6fa55d3ec42
mahurkarakshay242@gmail.com <mahurkarakshay242@gmail.com>
2019-12-04 15:46:32 +0530

    Creating model class/server integration

--- app/src/main/java/com/example/proficiency_exercise_app/ExerciseDetails.java
+++ app/src/main/java/com/example/proficiency_exercise_app/ExerciseDetails.java
@@ -1,4 +1,31 @@
 package com.example.proficiency_exercise_app;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class ExerciseDetails {
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("rows")
+    @Expose
+    private List<Row> rows = null;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Row> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<Row> rows) {
+        this.rows = rows;
+    }
 }
